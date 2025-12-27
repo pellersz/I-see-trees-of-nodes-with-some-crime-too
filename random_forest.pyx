495a6b60a7c3c63175e4aca7d78d8f5aab8d0940
Papp Gellert <pappgellert2003@gmail.com>
2025-12-27 13:11:18 +0200

    errors done with (for now)

--- random_forest.pyx
+++ random_forest.pyx
@@ -8,7 +8,9 @@ from functools import cache
 from ucimlrepo import fetch_ucirepo
 from random import randrange
 import cython
-from cython.mem import PyMem_Malloc, PyMem_Free
+from cpython.mem cimport PyMem_Malloc, PyMem_Free
+from cpython.ref cimport Py_INCREF, Py_DECREF, PyObject
+#from cpython.object cimport PyObject
 
 
 cdef class DecisionTree:
@@ -16,10 +18,10 @@ cdef class DecisionTree:
    
     cdef int is_categorical
     cdef int child_count
-    cdef DecisionTree* children
+    cdef PyObject** children
     cdef int** training_helper
     
-    cdef float divder
+    cdef float divider
     #TODO: get rid of this
     cdef float tmpdivider
 
@@ -65,10 +67,10 @@ cdef class DecisionTree:
             column = X[feature].copy().sort_values()
 
             for i in range(1, n - 1):
-                count[1][y[i - 1]] -= 1
-                count[1][number_of_labels] -= 1
-                count[0][y[i - 1]] += 1
-                count[0][number_of_labels] += 1
+                self.training_helper[1][y[i - 1]] -= 1
+                self.training_helper[1][number_of_labels] -= 1
+                self.training_helper[0][y[i - 1]] += 1
+                self.training_helper[0][number_of_labels] += 1
     
                 new_res = 0.0
 
@@ -87,7 +89,7 @@ cdef class DecisionTree:
 
                     if res < new_res:
                         res = new_res 
-                        self.tmpdivider = divder
+                        self.tmpdivider = divider
             
             return res
 
@@ -97,14 +99,17 @@ cdef class DecisionTree:
         cdef int* count 
         cdef int last_same = 0
         cdef int max_ind
+        cdef int max_category_count = 0
         cdef int best_feature_ind
         cdef float gain 
         cdef float best_gain
+        cdef DecisionTree new_child
 
         self.decision = -1
         self.children = NULL
+        self.child_count = 0
         self.training_helper = NULL
-        self.divder = -1.0
+        self.divider = -1.0
 
         if height_left <= 0:
             count = <int*> PyMem_Malloc(number_of_labels * sizeof(int));
@@ -129,8 +134,12 @@ cdef class DecisionTree:
             self.decision = y[0] 
             return
 
-        self.training_helper = <int**> PyMem_Malloc(category_count * sizeof(int*))
-        for i in range(category_count):
+        for i in range(len(features)):
+            if max_category_count < features[i][2]:
+                max_category_count = features[i][2]
+
+        self.training_helper = <int**> PyMem_Malloc(max_category_count * sizeof(int*))
+        for i in range(max_category_count):
             self.training_helper[i] = <int*> PyMem_Malloc((number_of_labels + 1) * sizeof(int))
 
         best_feature_ind = -1
@@ -138,65 +147,72 @@ cdef class DecisionTree:
         
         for i in range(len(features)):
             curr_feature = features[i]
-            gain = calculate_gain(X, y, curr_feature[0], curr_features[1], curr_features[2], number_of_labels)
+            gain = self.calculate_gain(X, y, curr_feature[0], curr_feature[1], curr_feature[2], number_of_labels)
             if gain > best_gain:
                 best_gain = gain
                 best_feature_ind = i
                 if not curr_feature[0]:
-                    self.divder = self.tmpdivider
+                    self.divider = self.tmpdivider
 
         best_feature = features[best_feature_ind]
         self.best_feature = best_feature[0]
         self.is_categorical = best_feature[1]
         self.child_count = best_feature[2]
-        self.children = <DecisionTree*> PyMem_Malloc(self.child_count * sizeof(DecisionTree))
+        self.children = <PyObject**> PyMem_Malloc(self.child_count * sizeof(PyObject*))
         features.pop(best_feature_ind)
     
         if self.is_categorical:
             indexes = []
-            for i in range(category_count):
+            for i in range(self.child_count):
                 indexes.append([])
 
             column = X[self.best_feature]
             for i in range(n):
                 indexes[column[i]].append(i)
 
-            for i in range(category_count):
+            for i in range(self.child_count):
                 if len(indexes[i]) != 0:
-                    self.children[i] = DecisionTree(X.reindex(indexes[i]), y.reindex(indexes[i]), features.copy(), height_left - 1, number_of_labels)
+                    new_child = DecisionTree(X.reindex(indexes[i]), y.reindex(indexes[i]), features.copy(), height_left - 1, number_of_labels)
+                    Py_INCREF(new_child)
+                    self.children[i] = <PyObject*> new_child
                 else:
                     self.children[i] = NULL
 
-        for i in range(category_count):
+        for i in range(max_category_count):
             PyMem_Free(self.training_helper)
         PyMem_Free(self.training_helper)
 
 
-    cdef int evaluate(x):
-        if decision != -1:
+    cdef int evaluate(self, x):
+        if self.decision != -1:
             return self.decision
         
-        if is_categorical:#might need an index 0
-            return children[x[self.best_feature]].evaluate(x)
+        if self.is_categorical:#might need an index 0
+            return (<DecisionTree> self.children[x[self.best_feature]]).evaluate(x)
         
-        return children[0].evaluate(x) if x[self.best_feature] < self.divider else children[1].evaluate(x)
+        return (<DecisionTree> self.children[0]).evaluate(x) if x[self.best_feature] < self.divider else (<DecisionTree> self.children[1]).evaluate(x)
 
 
     def __dealloc__(self):
+        for i in range(self.child_count):
+            if self.children[i] != NULL:
+                Py_DECREF(<object> self.children[i])
         PyMem_Free(self.children)
             
 
 cdef class RandomForest:
     cdef int tree_count
-    cdef DecisionTree* trees
+    cdef PyObject** trees
     cdef int* count
+    cdef DecisionTree new_tree
 
-    def __cinit__(self, X, y, features_names, int tree_count, int data_per_tree, int max_height, int number_of_labels):
+    def __cinit__(self, X, y, feature_names, int tree_count, int data_per_tree, int max_height, int number_of_labels):
         self.tree_count = tree_count
-        self.trees = <DecisionTree*> PyMem_Malloc(tree_count * sizeof(DecisionTree))
+        self.trees = <PyObject**> PyMem_Malloc(tree_count * sizeof(PyObject*))
         self.count = <int*> PyMem_Malloc(number_of_labels * sizeof(int))
         features = []
-        for i in range(len(features_names)):
+
+        for i in range(len(feature_names)):
             if type(X[feature_names[i][0]]) == int:
                 categories = set()
                 for category in X[feature_names[i]]:
@@ -208,28 +224,33 @@ cdef class RandomForest:
                 pass
         
         for i in range(tree_count):        
-            sample = [randrange(len(X)) for _ in randge(data_per_tree)]
+            sample = [randrange(len(X)) for _ in range(data_per_tree)]
             X_sample = X.reindex(sample)
             y_sample = y.reindex(sample)
-            self.trees[i] = DecisionTree(X, y, features, max_height, number_of_labels)
+            new_tree = DecisionTree(X, y, features, max_height, number_of_labels)
+            Py_INCREF(new_tree)
+            self.trees[i] = <PyObject*> new_tree
           
 
     cdef int evaluate(self, x):
         cdef int max_ind = 0
         for i in range(self.number_of_labels):
-            count[i] = 0
+            self.count[i] = 0
 
         for i in range(self.tree_count):
-            count[trees[i].evaluate(x)] += 1
+            self.count[(<DecisionTree> self.trees[i]).evaluate(x)] += 1
 
         for i in range(self.number_of_labels): 
-            if count[i] > count[max_ind]:
+            if self.count[i] > self.count[max_ind]:
                 max_ind = i
 
         return max_ind
 
 
     def __dealloc__(self):
+        PyMem_Free(self.count)
+        for i in range(self.tree_count):
+            Py_DECREF(<object> self.trees[i])
         PyMem_Free(self.trees)
-        PyMem_Free(self.counter)
+
 
